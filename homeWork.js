c93e15c227cf7f307aeb4aaa0e7e802dc8a23978
Nikita Dereviankin <n.dereviankin@sloboda-studio.com>
2024-01-25 16:02:17 +0200

    HomeWork 1.1

---
+++ homeWork.js
@@ -0,0 +1,18 @@
+//datatypes
+console.log(66)
+console.log(7.62)
+console.log("")
+console.log("hi");
+console.log('123')
+console.log(true)
+console.log(false)
+
+//comparison
+console.log(6 == '6')
+let first = {}
+let second = {}
+first = 1
+second = 1
+
+console.log(first == second)
+
